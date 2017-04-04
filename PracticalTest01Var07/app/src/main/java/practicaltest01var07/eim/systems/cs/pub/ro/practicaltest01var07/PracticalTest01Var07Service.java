9a12f01dc39357bb5a0d857a391422a79483a356
Student User <student@cti.pub.ro>
2017-04-04 09:42:23 +0300

    Last  commit
    Signed-off-by: Elena Mihailescu

--- PracticalTest01Var07/app/src/main/java/practicaltest01var07/eim/systems/cs/pub/ro/practicaltest01var07/PracticalTest01Var07Service.java
+++ PracticalTest01Var07/app/src/main/java/practicaltest01var07/eim/systems/cs/pub/ro/practicaltest01var07/PracticalTest01Var07Service.java
@@ -3,6 +3,7 @@ package practicaltest01var07.eim.systems.cs.pub.ro.practicaltest01var07;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class PracticalTest01Var07Service extends Service {
     public PracticalTest01Var07Service() {
@@ -10,6 +11,8 @@ public class PracticalTest01Var07Service extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.d("[Message]", "In onCommand in Service");
 
         int num1 = intent.getIntExtra("sum", 0);
 
